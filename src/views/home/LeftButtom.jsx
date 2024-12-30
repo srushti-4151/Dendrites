3d6e831654a94963f7417aa0eb46a6a77f5d78b1
srushti <srushtipatel4040@gmail.com>
2024-12-30 10:19:11 +0530

    new

--- src/views/home/LeftButtom.jsx
+++ src/views/home/LeftButtom.jsx
@@ -5,8 +5,8 @@ import pointimg from "../../assets/point1.webp";
 const LeftButtom = () => {
   return (
     <div className="bottom-left-div">
-      <div className="group border border-[#fff] w-[80px] h-[80px] lg:w-[110px] lg:h-[110px] rounded-full flex justify-center items-center cursor-pointer">
-        <div className="bg-[#00223e] group-hover:bg-white duration-1000 w-[60px] h-[60px] lg:w-[90px] lg:h-[90px] rounded-full flex justify-center items-center shadow">
+      <div className="group border border-[#fff] w-[90px] h-[90px] lg:w-[110px] lg:h-[110px] rounded-full flex justify-center items-center cursor-pointer">
+        <div className="bg-[#00223e] group-hover:bg-white duration-1000 w-[70px] h-[70px] lg:w-[90px] lg:h-[90px] rounded-full flex justify-center items-center shadow">
           <div className="relative w-full h-full flex justify-center items-center">
             <img src={brainimg} alt="brain" className="absolute" />
             <img

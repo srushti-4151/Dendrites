7885ef579ee943ae71ef3a1d5f60e9aac11a090e
srushti <srushtipatel4040@gmail.com>
2025-01-06 16:59:49 +0530

    product page bar

--- src/views/products/Bar.jsx
+++ src/views/products/Bar.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaChevronRight } from "react-icons/fa6";
 
 const Bar = () => {
+  const [topValue, setTopValue] = useState(110);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition <= 110) {
+      setTopValue(110 - scrollPosition);
+    } else {
+      setTopValue(70);
+    }
+  };
+
+  useEffect(() => {
+    console.log("topval", topValue);
+  
+  }, [topValue])
+  
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     
-    <div className="text-sm w-full bg-gray-100 bg-opacity-90 shadow-md py-3 fixed z-10 top-[70px]">
-      <div className="relative text-left w-full mx-auto px-4 flex space-x-3 text-gray-600 items-center">
-        <a href="/" className="text-gray-600 hover:underline">
+    <div className="text-sm w-full bg-gray-200 shadow-md py-3 fixed z-50"
+    style={{
+      top: window.innerWidth >= 1024 ? topValue : 0,
+    }}
+    >
+      <div className="relative text-base text-left w-full mx-auto px-4 flex space-x-3 text-gray-600 items-center">
+        <a href="/" className="text-gray-600">
           Home
         </a>
         <span className="text-lg">
           <FaChevronRight size={15} />
         </span>
-        <a href="/products" className="text-gray-600 hover:underline">
+        <a href="/products" className="text-gray-600">
           Products
         </a>
         <span>
           <FaChevronRight size={15} />
         </span>
-        <span className="text-blue-700">Product</span>
+        <span className="text-[#00223E]">Product</span>
       </div>
     </div>
    
